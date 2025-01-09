d0425f64ca7dd138fe642ba2ccec7854fa8b4316
Fritz J. Pichardo Marcano <fpichardom@yahoo.com>
2025-01-09 21:52:06 +0000

    Definition of custom fields with scheming

--- ckanext/biodigiknow/logic/validators.py
+++ ckanext/biodigiknow/logic/validators.py
@@ -1,5 +1,5 @@
 import ckan.plugins.toolkit as tk
-
+import uuid
 
 def biodigiknow_required(value):
     if not value or value is tk.missing:
@@ -7,7 +7,24 @@ def biodigiknow_required(value):
     return value
 
 
+# Definition based on ckanext-canada extension
+# https://github.com/open-data/ckanext-canada/blob/master/ckanext/canada/validators.py
+# 143:153
+def biodigiknow_validate_generate_uuid(value):
+    """
+    Accept UUID-shaped values or generate a uuid for this
+    dataset early so that it may be copied into the name field.
+    """
+    if not value or value is tk.missing:
+        return str(uuid.uuid4())
+    try:
+        return str(uuid.UUID(value))
+    except ValueError:
+        raise tk.Invalid(tk._("Badly formed hexadecimal UUID string"))
+
+
 def get_validators():
     return {
         "biodigiknow_required": biodigiknow_required,
+        "biodigiknow_validate_generate_uuid": biodigiknow_validate_generate_uuid,
     }
--- ckanext/biodigiknow/plugin.py
+++ ckanext/biodigiknow/plugin.py
@@ -1,13 +1,17 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from collections import OrderedDict
+import json
+import os
 
 
 # import ckanext.biodigiknow.cli as cli
 # import ckanext.biodigiknow.helpers as helpers
 # import ckanext.biodigiknow.views as views
-# from ckanext.biodigiknow.logic import (
+from ckanext.biodigiknow.logic import (
+    validators
 #     action, auth, validators
-# )
+)
 
 
 class BiodigiknowPlugin(plugins.SingletonPlugin):
@@ -18,13 +22,59 @@ class BiodigiknowPlugin(plugins.SingletonPlugin):
     # plugins.implements(plugins.IBlueprint)
     # plugins.implements(plugins.IClick)
     # plugins.implements(plugins.ITemplateHelpers)
-    # plugins.implements(plugins.IValidators)
-    
+    plugins.implements(plugins.IValidators)
+    plugins.implements(plugins.IFacets)
+    plugins.implements(plugins.IPackageController)
+
+    # List of fields that should be processed as JSON lists
+    # Add fields as needed
+    JSON_LIST_FIELDS = [
+        'task_cluster',
+        'task',
+        # Add more fields here
+    ]
+
+    def _process_json_list_field(self, value, field_name='unknown'):
+        """
+        Process a field that should be a JSON list of strings
+        
+        Args:
+            value: The value to process (string or list)
+            field_name: Name of field for logging purposes
+            
+        Returns:
+            list: Processed list of strings
+        """
+        print(f"Incoming {field_name}: {value} of type {type(value)}")
+        
+        try:
+            # If it's a JSON string, parse it
+            if isinstance(value, str) and value.strip().startswith('['):
+                items = json.loads(value)
+            # If it's already a list, use it as is
+            elif isinstance(value, list):
+                items = value
+            else:
+                items = []
+
+            # Ensure all items are strings and remove empty values
+            items = [str(item).strip() for item in items if item]
+            
+            print(f"Processed {field_name}: {items}")
+            return items
+            
+        except json.JSONDecodeError:
+            print(f"Error parsing JSON for {field_name}: {value}")
+            return []
+        except Exception as e:
+            print(f"Unexpected error processing {field_name}: {str(e)}")
+            return []
 
     # IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
+        print("Registering public directory:", os.path.join(os.path.dirname(__file__), 'public'))
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "biodigiknow")
 
@@ -56,6 +106,74 @@ class BiodigiknowPlugin(plugins.SingletonPlugin):
 
     # IValidators
 
-    # def get_validators(self):
-    #     return validators.get_validators()
+    def get_validators(self):
+        return validators.get_validators()
+
+    # IFacets
+
+    def dataset_facets(self, facets_dict, package_type):
+        """Modify the facets_dict and return it
+        """
+        new_facets = OrderedDict()
+        new_facets['type'] = toolkit._('Digitization Resource Type')
+        new_facets['task_cluster'] = toolkit._('Task Clusters')
+        new_facets['task'] = toolkit._('Tasks')
+
+        # Add the rest of the facets
+        for key, value in facets_dict.items():
+            new_facets[key] = value
+
+        return new_facets
+    
+    def group_facets(self, facets_dict, group_type, package_type):
+        return facets_dict
+    
+    def organization_facets(self, facets_dict, organization_type, package_type):
+        return facets_dict
+
+    def before_dataset_index(self, pkg_dict):
+        # Get the task_cluster value
+
+        for field_name in self.JSON_LIST_FIELDS:
+            if field_name in pkg_dict:
+                pkg_dict[field_name] = self._process_json_list_field(
+                    pkg_dict.get(field_name, ''),
+                    field_name
+                )
+        return pkg_dict
+   
+    # Required methods with default implementations
+    
+    def before_dataset_search(self, search_params):
+        return search_params
+
+    def after_dataset_search(self, search_results, search_params):
+        return search_results
+
+    def before_dataset_view(self, pkg_dict):
+        return pkg_dict
+
+    def after_dataset_create(self, context, pkg_dict):
+        return pkg_dict
+
+    def after_dataset_update(self, context, pkg_dict):
+        return pkg_dict
+
+    def after_dataset_delete(self, context, pkg_dict):
+        return pkg_dict
+
+    def after_dataset_show(self, context, pkg_dict):
+        return pkg_dict
+
+    def create(self, entity):
+        pass
+
+    def edit(self, entity):
+        pass
+
+    def delete(self, entity):
+        pass
+
+    def read(self, entity):
+        pass
     
